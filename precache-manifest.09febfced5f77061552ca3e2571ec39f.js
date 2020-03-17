e495df19ff2334ff7d1ead67c94fb02f0109cff8
Arie Shapiro <ran.shapiro@atos.net>
2020-03-17 11:55:23 +0100

    Updates

---
+++ precache-manifest.09febfced5f77061552ca3e2571ec39f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2394099529df8424dbedd5e6f5129346",
+    "url": "/javascript-calculator/index.html"
+  },
+  {
+    "revision": "02ffece0fe47d24b5a50",
+    "url": "/javascript-calculator/static/css/main.8972e449.chunk.css"
+  },
+  {
+    "revision": "ca83442badc0021a3e7d",
+    "url": "/javascript-calculator/static/js/2.af173df1.chunk.js"
+  },
+  {
+    "revision": "02ffece0fe47d24b5a50",
+    "url": "/javascript-calculator/static/js/main.d4cd80fa.chunk.js"
+  },
+  {
+    "revision": "af3dc86e0aee640c7c09",
+    "url": "/javascript-calculator/static/js/runtime~main.b6bacda3.js"
+  }
+]);
